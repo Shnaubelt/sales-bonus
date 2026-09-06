b15323a428b50e9fef49c0be9f7cb6dfd7f698bb
Shnaubelt <shnaubelte@mail.ru>
2026-09-07 00:35:21 +0300

    сделала основной цикл обработки

--- src/main.js
+++ src/main.js
@@ -5,7 +5,11 @@
  * @returns {number}
  */
 function calculateSimpleRevenue(purchase, _product) {
-   // @TODO: Расчет выручки от операции
+  // @TODO: Расчет выручки от операции
+  const { discount, sale_price, quantity } = purchase;
+  const discountCoefficient = 1 - (purchase.discount / 100);
+  const revenue = sale_price*quantity*discountCoefficient;
+  return revenue;
 }
 
 /**
@@ -16,29 +20,116 @@ function calculateSimpleRevenue(purchase, _product) {
  * @returns {number}
  */
 function calculateBonusByProfit(index, total, seller) {
-    // @TODO: Расчет бонуса от позиции в рейтинге
+  // @TODO: Расчет бонуса от позиции в рейтинге
+  const { profit } = seller;
 }
 
 /**
  * Функция для анализа данных продаж
- * @param data
- * @param options
+ * @param data данные
+ * @param options две прошлые функции
  * @returns {{revenue, top_products, bonus, name, sales_count, profit, seller_id}[]}
  */
 function analyzeSalesData(data, options) {
-    // @TODO: Проверка входных данных
+  // @TODO: Проверка входных данных
+  if (!data) {
+    throw new Error("Некорректные входные данные");
+  }
 
-    // @TODO: Проверка наличия опций
+  if (
+    !data.customers ||
+    !Array.isArray(data.customers) ||
+    data.customers.length === 0
+  ) {
+    throw new Error("Некорректные данные покупателей");
+  }
 
-    // @TODO: Подготовка промежуточных данных для сбора статистики
+  if (
+    !data.products ||
+    !Array.isArray(data.products) ||
+    data.products.length === 0
+  ) {
+    throw new Error("Некорректные данные продуктов");
+  }
 
-    // @TODO: Индексация продавцов и товаров для быстрого доступа
+  if (
+    !data.sellers ||
+    !Array.isArray(data.sellers) ||
+    data.sellers.length === 0
+  ) {
+    throw new Error("Некорректные данные продавцов");
+  }
 
-    // @TODO: Расчет выручки и прибыли для каждого продавца
+  if (
+    !data.purchase_records ||
+    !Array.isArray(data.purchase_records) ||
+    data.purchase_records.length === 0
+  ) {
+    throw new Error("Некорректные данные продаж");
+  }
+  // @TODO: Проверка входных данных
 
-    // @TODO: Сортировка продавцов по прибыли
+  // @TODO: Проверка наличия опций
+  const { calculateRevenue, calculateBonus } = options;
 
-    // @TODO: Назначение премий на основе ранжирования
+  if (typeof calculateRevenue !== "function") {
+    throw new Error("опция calculateRevenue не является функцией");
+  }
 
-    // @TODO: Подготовка итоговой коллекции с нужными полями
+  if (typeof calculateBonus !== "function") {
+    throw new Error("опция calculateBonus не является функцией");
+  }
+  // @TODO: Проверка наличия опций
+
+  // @TODO: Подготовка промежуточных данных для сбора статистики
+  const sellerStats = data.sellers.map((seller) => ({
+    ...seller,
+    revenue: 0,
+    top_products: 0,
+    bonus: 0,
+    sales_count: 0,
+    profit: 0,
+  }));
+  // @TODO: Подготовка промежуточных данных для сбора статистики
+
+  // @TODO: Индексация продавцов и товаров для быстрого доступа
+  const sellerIndex = Object.fromEntries(
+    sellerStats.map((seller) => [seller.id, seller]),
+  );
+
+  const productIndex = Object.fromEntries(
+    data.products.map((product) => [product.sku, product]),
+  );
+  // @TODO: Индексация продавцов и товаров для быстрого доступа
+
+  // @TODO: Расчет выручки и прибыли для каждого продавца
+  data.purchase_records.forEach((record) => {
+    const seller = sellerIndex[record.seller_id];
+    seller.sales_count += 1;
+    seller.revenue +=record.total_amount;
+
+    record.items.forEach((item) => {
+      const product = productIndex[item.sku];
+      const cost = product.purchase_price*item.quantity;
+      const revenue = calculateRevenue(item, product);
+      const profit = revenue - cost;
+      seller.profit += profit;
+
+      if (!seller.products_sold) {
+        seller.products_sold = {};
+      }
+      if (!seller.products_sold[item.sku]) {
+        seller.products_sold[item.sku] = 0;
+      }
+      seller.products_sold[item.sku] += item.quantity;
+    });
+  });
+  
+  // @TODO: Расчет выручки и прибыли для каждого продавца
+
+  // @TODO: Сортировка продавцов по прибыли
+
+  // @TODO: Назначение премий на основе ранжирования
+
+  // @TODO: Подготовка итоговой коллекции с нужными полями
 }
